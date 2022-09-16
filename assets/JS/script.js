229dd44c6c1912c2ac45ba5ff96af45869d42bfe
breakfireaus <myoungerc@hotmail.com>
2022-09-16 10:23:15 +1000

    psuedo added to html and js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,24 @@
+// add a favicon that changes based on the weather
+
+// Weather refreshes on a set Interval
+// Maybe add some autolocation...refresh the time and date
+
+// user can search for a City
+// city can be added to search history
+// if clicked the same info can be viewed again
+
+// city results will display the current conditions and future conditions
+
+// display current city name, the date, an icon that represents the weather conditions, 
+// the temperature, humidity, wind speed, and the UV index in a box
+
+// data displayed for a 5-day forecast is the date, weather conditions icon, temp, windspeed and humidity
+
+// City is added to a search history array
+// Need to be saved locally
+// delete button for search history
+
+// Cities in search history can be selected.
+// Once selected the same info can be viewed again.
+// Consider a delete button for search history
 
